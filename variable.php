fd79a9abca4f6d140713babfefe2ca360084555e
Abdur Rashid Sawom <asawom250@gmail.com>
2025-02-05 08:05:33 +0600

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+    $age = 27;
+    $food = 'pizza';
+
+    echo 'my age is = ' .$age . '<br>'; 
+    // . mane concatenation
+    echo 'food = ' .$food . '<br>';
+    
+    $num1 = 10;
+    $num2 = 69;
+    $add = $num1 + $num2;
+    echo ' result = ' .$add . '<br>' .'<br>';
+
+    $num3 = 10.69;
+    $num4 = 69.96;
+    $add2 = $num3 + $num4;
+    echo ' result = ' .$add2;
+
+    ?> 
+</body>
+</html>
