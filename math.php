fed29ea90ebfa244e8eda56c6a489d3fbcfdd797
Abdur Rashid Sawom <asawom250@gmail.com>
2025-02-08 23:43:52 +0600

    Add files via upload

---
+++ math.php
@@ -0,0 +1,25 @@
+<?php
+$number = array(10,2,45,8,47,120,9,3);
+echo 'max = ';
+echo ( max($number) );
+
+echo '<br><br>';
+
+echo 'min = ';
+echo ( min($number) );
+
+echo '<br><br>';
+
+echo 'round number = ';
+$roundNumber = 3.56;
+echo(round($roundNumber)) ;
+
+
+echo '<br><br>';
+echo 'abs = ';
+echo(abs($roundNumber)) ;
+
+echo '<br><br>';
+echo 'random = ';
+echo(rand(1,6)) ;
+?>
